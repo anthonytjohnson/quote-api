7ed136368ac89b788dd81361ccd7d3496665ffc2
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-10-15 16:03:31 +0900

    added get requests for quotes

--- server.js
+++ server.js
@@ -7,6 +7,23 @@ const { getRandomElement } = require('./utils');
 const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
+app.get('/api/quotes/random', (req, res) => {
+  res.send({
+    quote: getRandomElement(quotes)
+  });
+});
+app.get('/api/quotes', (req, res) => {
+  if (req.query.person !== undefined) {
+    const quotesByPerson = quotes.filter(quote => quote.person === req.query.person);
+    res.send({
+      quotes: quotesByPerson
+    });
+  } else {
+    res.send({
+      quotes: quotes
+    });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
